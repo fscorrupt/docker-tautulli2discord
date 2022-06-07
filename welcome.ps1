e7a8f5c0b1c270298e10f530038c87b02ee5e30f
FSCorrupt <45659314+fscorrupt@users.noreply.github.com>
2022-06-07 09:32:20 +0200

    Update welcome.ps1

--- welcome.ps1
+++ welcome.ps1
@@ -3,7 +3,10 @@ cls
 $scripts =  (get-childitem -Filter *.ps1 | where name -ne 'welcome.ps1').Name.replace('.ps1','')
 
 Write-Host "Currently there are '$($scripts.count)' Scripts integrated" -ForegroundColor Yellow
-Write-Host "##############################################" -ForegroundColor Green
+Write-Host  ''
+Write-Host "Example on how to run them:" -ForegroundColor Yellow
+Write-Host "docker exec -it mcr.microsoft.com/powershell pwsh PlexLibraryStats.ps1" -ForegroundColor Cyan
+Write-Host "####################################################" -ForegroundColor Green
 
 foreach ($script in $scripts){
   write-host $script -ForegroundColor Cyan
