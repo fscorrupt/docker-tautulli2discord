b67efcd3cdc2116f6ce6d62ac51fbd0da69f03f6
FSCorrupt <45659314+fscorrupt@users.noreply.github.com>
2022-06-07 09:24:35 +0200

    Update welcome.ps1

--- welcome.ps1
+++ welcome.ps1
@@ -3,7 +3,7 @@ cls
 $scripts =  (get-childitem -Filter *.ps1 | where name -ne 'welcome.ps1').Name.replace('.ps1','')
 
 Write-Host "Currently there are '$($scripts.count)' Scripts integrated" -ForegroundColor Yellow
-Write-Host "####################################################" -ForegroundColor Green
+Write-Host "##############################################" -ForegroundColor Green
 
 foreach ($script in $scripts){
   write-host $script -ForegroundColor Cyan
