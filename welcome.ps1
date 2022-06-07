88f7c9447b22d0c6ca62c9faf0661ee6e28bad70
FSCorrupt <45659314+fscorrupt@users.noreply.github.com>
2022-06-07 08:58:14 +0200

    Update welcome.ps1

--- welcome.ps1
+++ welcome.ps1
@@ -1,8 +1,11 @@
 # Show integraded Scripts
-$scripts =  (get-childitem -Filter *.ps1).Name
+$scripts =  (get-childitem -Filter *.ps1 | where name -ne 'welcome.ps1').Name.replace('.ps1','')
 
 Write-Host "################################################"
 Write-Host "#Currently there are '$($scripts.count)' Scripts integrated#"
 Write-Host "################################################"
 
 Write-Host $scripts
+
+
+
