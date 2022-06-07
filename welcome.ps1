8e813a5d639cb670a4c11ff43285f2b7aca4e3c5
FSCorrupt <45659314+fscorrupt@users.noreply.github.com>
2022-06-07 09:46:27 +0200

    Update welcome.ps1

--- welcome.ps1
+++ welcome.ps1
@@ -1,4 +1,9 @@
-cls
+function recurse { 
+  sleep 60
+  echo "Running"
+  recurse
+}
+
 # Show integraded Scripts
 $scripts =  (get-childitem -Filter *.ps1 | where name -ne 'welcome.ps1').Name.replace('.ps1','')
 
@@ -6,11 +11,13 @@ Write-Host "Currently there are '$($scripts.count)' Scripts integrated" -Foregro
 Write-Host  ''
 Write-Host "First fill out Config here: " -ForegroundColor Yellow -NoNewline
 Write-Host "/opt/appdata/tautulli2discord/config/config.json" -ForegroundColor Cyan 
-Write-Host  ''
-Write-Host "Example on how to run them:" -ForegroundColor Yellow
+Write-Host "Example on how to run them:" -ForegroundColor Yellow -NoNewline
 Write-Host "docker exec -it tautulli2discord pwsh PlexLibraryStats.ps1" -ForegroundColor Cyan
 Write-Host "####################################################" -ForegroundColor Green
 
 foreach ($script in $scripts){
   write-host $script -ForegroundColor Cyan
 }
+
+# Call Recursive Function.
+recurse
