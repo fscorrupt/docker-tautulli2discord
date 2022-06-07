9f248d301d55450dc1d0919e6bcdb30c040905f0
FSCorrupt <45659314+fscorrupt@users.noreply.github.com>
2022-06-07 10:19:15 +0200

    Update welcome.ps1

--- welcome.ps1
+++ welcome.ps1
@@ -1,8 +1,8 @@
 function recurse {
   sleep 180
-  cls
   $elapsedTime = $(get-date) - $StartTime
   $totalTime = $elapsedTime.Days.ToString() +' Days '+ $elapsedTime.Hours.ToString() +' Hours '+ $elapsedTime.Minutes.ToString() +' Min ' + $elapsedTime.Seconds.ToString() +' Sec'
+  write-host ""
   write-host "Container is running since: " -NoNewline
   write-host "$totalTime" -ForegroundColor Cyan
   recurse
@@ -16,9 +16,9 @@ $scripts =  (get-childitem -Filter *.ps1 | where name -ne 'welcome.ps1').Name.re
 
 Write-Host "Currently there are '$($scripts.count)' Scripts integrated" -ForegroundColor Yellow
 Write-Host  ''
-Write-Host "First fill out Config here: " -ForegroundColor Yellow -NoNewline
+Write-Host "First fill out config here: " -ForegroundColor Yellow -NoNewline
 Write-Host "/opt/appdata/tautulli2discord/config/config.json" -ForegroundColor Cyan 
-Write-Host "Example on how to run them: " -ForegroundColor Yellow -NoNewline
+Write-Host "Example on how to run the Script: " -ForegroundColor Yellow -NoNewline
 Write-Host "docker exec -it tautulli2discord pwsh PlexLibraryStats.ps1" -ForegroundColor Cyan
 Write-Host "####################################################" -ForegroundColor Green
 
